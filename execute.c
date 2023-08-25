26fdee146f9b81ed2b65315b32238c54d839048f
habeeb <bidehassan@gmail.com>
2023-08-24 20:54:43 -0700

    update exec file

--- execute.c
+++ execute.c
@@ -10,21 +10,23 @@
 int execute(char *content, stack_t **stack, unsigned int counter, FILE *file)
 {
 	instruction_t opst[] = {
-				{"push", f_push}, {"pall", f_pall}, {"pint", f_pint},
-				{"pop", f_pop},
-				{"swap", f_swap},
-				{"add", f_add},
+				{"push", m_push}, 
+				{"pall", f_pall}, 
+				{"pint", m_printop},
+				{"pop", m_pop},
+				{"swap", m_replace},
+				{"add", f_sum},
 				{"nop", f_nop},
-				{"sub", f_sub},
-				{"div", f_div},
+				{"sub", m_subtract},
+				{"div", quotient},
 				{"mul", f_mul},
 				{"mod", f_mod},
-				{"pchar", f_pchar},
-				{"pstr", f_pstr},
-				{"rotl", f_rotl},
-				{"rotr", f_rotr},
-				{"queue", f_queue},
-				{"stack", f_stack},
+				{"pchar", m_mchar},
+				{"pstr", m_prinstr},
+				{"rotl", m_rotst},
+				{"rotr", m_rotstb},
+				{"queue", m_queue},
+				{"stack", m_stack},
 				{NULL, NULL}
 				};
 	unsigned int i = 0;
